19976d8635b8cf6c464da89efaad489d24796559
Francesco <samplasion@gmail.com>
2020-12-02 10:12:15 +0100

    Create a main runner file

---
+++ 2020/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "==========================="
+echo "= Samplasion's AoC runner ="
+echo "==========================="
+echo
+
+day="$([[ -p /dev/stdin ]] && cat - || echo "$@")"
+if [[ -z "$day" ]]; then
+    read -p "What day's challenge do you want to run? [1-25] Please enter a number without leading zeros: " day
+else
+    echo "Running the solution for day ${day}..."
+fi
+
+echo
+
+cd "./day $day"
+./run.sh
+cd ..
