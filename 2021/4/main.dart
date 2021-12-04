672fabde710b67c8892d96678a3f6fecf1bf8b2c
Francesco <samplasion@gmail.com>
2021-12-04 09:43:00 +0100

    Add copyright notice

--- 2021/4/main.dart
+++ 2021/4/main.dart
@@ -1,3 +1,8 @@
+// Copyright (c) 2021 Samplasion <samplasion@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 import 'dart:io';
 
 main() async {
